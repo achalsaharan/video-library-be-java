4e24e38379cce2237c5583469a010fb8292fc21e
achal saharan <asaharan_be17@thapar.edu>
2024-05-21 22:37:29 +0530

    feat: add repositories

---
+++ src/main/java/com/example/videolibrarybe/repository/VideoRepository.java
@@ -0,0 +1,6 @@
+package com.example.videolibrarybe.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VideoRepository extends CrudRepository<VideoRepository, String> {
+}
